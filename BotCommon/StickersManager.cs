3f8a4733656b57371c0cb276358692b404f477d4
Dezodemius <gladkovyegor@gmail.com>
2024-08-03 14:00:19 +0400

    glow up sticker manager

--- BotCommon/StickersManager.cs
+++ BotCommon/StickersManager.cs
@@ -4,19 +4,36 @@ using Telegram.Bot.Types;
 
 namespace BotCommon;
 
+/// <summary>
+/// Telegram chat sticker manager.
+/// </summary>
 public static class StickersManager
 {
+  /// <summary>
+  /// Sticker from pack.
+  /// </summary>
   private static Sticker[] _stickers;
 
-  public static void InitializeStickerPack(ITelegramBotClient bot, string stickerPackName)
+  /// <summary>
+  /// Initialize manager by sticker pack.
+  /// </summary>
+  /// <param name="botClient">Telegram bot client.</param>
+  /// <param name="stickerPackName">Sticker pack name.</param>
+  public static void InitializeStickerPack(ITelegramBotClient botClient, string stickerPackName)
   {
-    _stickers = bot.GetStickerSetAsync(stickerPackName).Result.Stickers;
+    _stickers = botClient.GetStickerSetAsync(stickerPackName).Result.Stickers;
   }
 
-  public static async void SendStickerAsync(ITelegramBotClient bot, long chatId, string stickerEmoji)
+  /// <summary>
+  /// Send sticker from sticker pack.
+  /// </summary>
+  /// <param name="botClient">Telegram bot client.</param>
+  /// <param name="chatId">Telegram user chat ID.</param>
+  /// <param name="stickerEmoji">Emoji that represents sticker.</param>
+  public static async void SendStickerAsync(ITelegramBotClient botClient, long chatId, string stickerEmoji)
   {
     if (_stickers == null)
       return;
-    await bot.SendStickerAsync(chatId, new InputFileId(_stickers.First(x => x.Emoji == stickerEmoji).FileId));
+    await botClient.SendStickerAsync(chatId, new InputFileId(_stickers.First(x => x.Emoji == stickerEmoji).FileId));
   }
 }
